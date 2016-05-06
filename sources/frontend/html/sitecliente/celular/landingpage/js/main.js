a47d8e9b7f91ee1a53c517eee926d7bbfc8a2a73
fernando.basilio <fernando.basilio@wunderman.com>
2016-05-06 18:35:31 -0300

    corrigindo ultimos bugs de JS

--- sources/frontend/html/sitecliente/celular/landingpage/js/main.js
+++ sources/frontend/html/sitecliente/celular/landingpage/js/main.js
@@ -1,12 +1,12 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-header();
+// header();
 var wrap = document.querySelector('#wrap');
 var content = document.querySelector('.pm-content');
 var loader = document.querySelector('.loader');
 
-loader.style.display = 'none';
-content.style.display = 'block';
-wrap.style.display = 'block';
+// loader.style.display = 'none';
+// content.style.display = 'block';
+// wrap.style.display = 'block';
 
 // EXEMPLO DE MÓDULOS
 
